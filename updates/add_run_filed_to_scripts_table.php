f5ffbbbbfa91d8bc86c8859cabe4ad944b63a5f1
jc91715 <370234915@qq.com>
2019-07-23 11:53:58 +0800

    实时运行

---
+++ updates/add_run_filed_to_scripts_table.php
@@ -0,0 +1,20 @@
+<?php namespace RV\PhpConsole\Updates;
+
+use Schema;
+use October\Rain\Database\Schema\Blueprint;
+use October\Rain\Database\Updates\Migration;
+
+class AddRunFiledToScriptsTable extends Migration
+{
+    public function up()
+    {
+        Schema::table('rv_phpconsole_scripts', function(Blueprint $table) {
+            $table->tinyInteger('if_run')->default(1);
+        });
+    }
+
+    public function down()
+    {
+
+    }
+}
